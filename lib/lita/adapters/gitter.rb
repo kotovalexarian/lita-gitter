a019c4f0d6a0574de1ee76baa202bbc706760ae3
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-20 03:27:59 +0500

    Add documentation

--- lib/lita/adapters/gitter.rb
+++ lib/lita/adapters/gitter.rb
@@ -1,5 +1,14 @@
+##
+# Lita module.
+#
 module Lita
+  ##
+  # Lita adapters module.
+  #
   module Adapters
+    ##
+    # Gitter adapter for the Lita chat bot.
+    #
     class Gitter < Adapter
     end
 
