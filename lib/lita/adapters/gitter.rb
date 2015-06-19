6053ebf2a520ceada60d94eb7c73508486cd411c
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-20 03:36:13 +0500

    Implement adapter configuration

--- lib/lita/adapters/gitter.rb
+++ lib/lita/adapters/gitter.rb
@@ -10,6 +10,8 @@ module Lita
     # Gitter adapter for the Lita chat bot.
     #
     class Gitter < Adapter
+      config :token,   type: String, required: true
+      config :room_id, type: String, required: true
     end
 
     Lita.register_adapter(:gitter, Gitter)
