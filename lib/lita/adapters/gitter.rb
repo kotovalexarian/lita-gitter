82eb82d817eb5fcafcd0c1b63c41f0b955cf9296
Braiden Vasco <braiden-vasco@mailtor.net>
2015-06-20 04:46:11 +0500

    Implement main loop in Lita::Adapters::Gitter#run

--- lib/lita/adapters/gitter.rb
+++ lib/lita/adapters/gitter.rb
@@ -1,3 +1,6 @@
+require 'em-http'
+require 'json'
+
 ##
 # Lita module.
 #
@@ -12,6 +15,37 @@ module Lita
     class Gitter < Adapter
       config :token,   type: String, required: true
       config :room_id, type: String, required: true
+
+      # The main loop. Listens for incoming messages,
+      # creates {Lita::Message} objects from them,
+      # and dispatches them to the robot.
+      #
+      def run # rubocop:disable Metrics/MethodLength
+        stream_url =
+          "https://stream.gitter.im/v1/rooms/#{config.room_id}/chatMessages"
+
+        http = EventMachine::HttpRequest.new(
+          stream_url,
+          keepalive: true,
+          connect_timeout: 0,
+          inactivity_timeout: 0,
+        )
+
+        EventMachine.run do
+          request = http.get(
+            head: {
+              'Accept' => 'application/json',
+              'Authorization' => "Bearer #{config.token}",
+            }
+          )
+
+          request.stream do |chunk|
+            unless chunk.strip.empty?
+              text = JSON.parse(chunk)['text']
+            end
+          end
+        end
+      end
     end
 
     Lita.register_adapter(:gitter, Gitter)
